2a6f120a851c13076657381e64e12df7e13ec5ee
Lauden Eller <laudlauden@gmail.com>
2022-03-15 13:52:05 -0700

    Vendor form still not sending correct objects to Json

--- src/components/vendors/VendorForm.js
+++ src/components/vendors/VendorForm.js
@@ -1,70 +1,80 @@
-// This module will display a new vendor form that will save new vendor, product and vendorProduct objects to Json
-
-// DO NOT KNOW WHY THE DEBUGGER CLEARS THE NETWORK WINDOW WHEN THE ADD VENDOR BUTTON IS CLICKED: NEED ACCESS TO SEE DATA!
-
+// This module will display a new vendor form that will save new vendor, product, and vendorProduct objects to Json
 import React, { useState, useEffect } from "react";
-import { getAllProducts, getAllVendors } from "../json/ApiManger";
-// import { VendorProducts } from "./VendorProducts";
+import { getAllProducts } from "../json/ApiManger";
 
-// export a function that returns html for a form
+// export a function that returns Jsx for a form
 export const VendorForm = () => {
 
-    // Initiate useState
+    // Initiate useStates
     const [vendor, addVendors] = useState([])
     const [products, setProducts] = useState([])
+    const [updatedProducts, setUpdatedProducts] = useState([])
+    // Stores the response from the POST vendor function so vendorProducts can access the id
+    const [vendorResponse, setVendorResponse] = useState({})
+    // Stores a copy of the new products created in the addProducts function so vendorProducts can access the ids
+    const [newProductArray, setNewProductArray] = useState([])
     const [newProductList, setProductList] = useState([
         { product: "" }
-    ])
-
+    ]) // newProductList is pre-loaded with an object because the form iterates over that array to build the input fields
+    
+    // These handle functions allow the user to add and remove input fields for products
     const handleProductAdd = () => {
         setProductList([...newProductList, { product: "" }])
     }
-
+    
     const handleProductRemove = (index) => {
         const list = [...newProductList]
         list.splice(index, 1)
         setProductList(list)
     }
-
+    
     const handleProductChange = (e, index) => {
         const { name, value } = e.target
         const list = [...newProductList]
         list[index][name] = value
         setProductList(list)
     }
+    
+    // updatedProducts changes when its' set function is invoked on line 61
+    useEffect(
+        () => {
+            addVendorProducts()
+        },
+        [updatedProducts]
+        )
 
-    // Save empty objects to module-scope variables for storing the object which comes back in the response from the post functions
-    let productResponse = {}
-    let vendorResponse = {}
+    // Declare a string of function invocations within a .then() chain so the fetches are resolved before the next one begins
 
-    // Declare a string of function invocations within a .then chain so the fetches are resolved before the next one begins
-    const updateJson = () => {
-
+        // BELIEVE I CAN REMOVE THE SECOND GET PRODUCTS FUNCTION BECAUSE I NO LONGER NEED TO DO THE SECOND CHECK IN ADDVENDOR FUNCTION
+    const updateJson = (event) => {
+        
+        // Stops the DOM from clearing the form
+        event.preventDefault()
+        
+        // .THEN() FUNCTIONS ACCEPT AND INVOKE A CALLBACK FUNCTION
         addNewVendor()
-            .then((newVendorResponse) => {
-                newVendorResponse = vendorResponse
-                getAllProducts()
-                    .then((data) => {
-                        setProducts(data)
-                    })
+            .then(setVendorResponse)
+            .then(getAllProducts)
+            .then(setProducts)
+            .then(() => Promise.all(addProducts()))
+            .then((data) => setNewProductArray(data))
+            .then(getAllProducts)
+            .then((data) => {
+                setProducts(data)
+                setUpdatedProducts(data)
             })
-            .then(addProducts())
-            .then((newProductResponse) => {
-                newProductResponse = productResponse
-                addVendorProducts()
-            })
-    }
+            .then(() => {console.log(newProductArray)})
+        }
 
     // Send new vendor object to Json database
     const addNewVendor = () => {
+        
         const newVendor = {
             // Add keys to the object that match the user input
             address: vendor.address,
             name: vendor.name,
             userId: parseInt(localStorage.getItem("groce_user"))
         }
-        // // Use .prventDefault() to stop default event behaviour from the DOM
-        // event.preventDefault()
 
         // Post new object to Json
         const fetchOption = {
@@ -78,32 +88,34 @@ export const VendorForm = () => {
         return fetch("http://localhost:8088/vendors", fetchOption)
             .then(r => r.json())
     }
-    // NEW PRODUCTS ARE BEING CREATED BUT THE CHECK IS NOT WORKING
+    // Checks to see if the new products from the form already exist in Json, 
+        // and if not, sends new product objects to Json
     const addProducts = () => {
+        // NEW PRODUCTS ARE BEING CREATED BUT THE CHECK IS NOT WORKING
 
-        // CREATE CHECK TO SEE IF INPUTTED PRODUCTS ALREADY EXIST FOR CURRENT USER
+        // Checks to see if the inputted products already exist in Json
         const checkProduct = () => {
 
-            // ITERATE THROUGH DATA
-            products.map(product => {
-                // COMPARE NEW PRODUCT TO EXISTING DATABASE
-                newProductList.map(newProduct => {
-                    if (product.description === newProduct.product && product.userId === parseInt(localStorage.getItem("groce_user"))) {
-                        // RETURN TRUE OR FALSE
+            for (const product of products) {
+
+                for (const newProduct of newProductList) {
+
+                    if (product.description === newProduct.description && product.userId === parseInt(localStorage.getItem("groce_user"))) {
+                        
                         return true
                     }
-                })
-            })
+                }
+            }
         }
 
-        newProductList.map(newProduct => {
+        // Returns an array of Json responses (one for each POST products function)
+        return newProductList.map(newProduct => {
             if (checkProduct === true) {
-                console.log("product exists")
+                console.log("product already exists in database")
             }
-            // IF NOT, CREATE NEW PRODUCTS
             else {
                 const jsonProduct = {
-                    description: newProduct.product,
+                    description: newProduct.description,
                     userId: parseInt(localStorage.getItem("groce_user"))
                 }
 
@@ -121,29 +133,33 @@ export const VendorForm = () => {
         })
     }
 
-    // NEW VENDORPROdUCTS ARE BEING CREATED BUT THEY DO NOT HAVE THE CORRECT KEYS
-    const addVendorProducts = () => {
+    // ONLY SENDING ONE VENDORPRODUCT TO JSON
+    const addVendorProducts = () => { 
 
-        newProductList.map(() => {
+        // for (const product of newProductArray) {
+        for (const product of updatedProducts) {
+            for (const newProduct of newProductArray) {
+                        if (product.description === newProduct.product &&
+                            product.userId === parseInt(localStorage.getItem("groce_user"))) {
 
-            const newVendorProduct = {
-                vendorId: vendorResponse.id,
-                productId: productResponse.Id,
-                userId: parseInt(localStorage.getItem("groce_user"))
-            }
+                            const newVendorProduct = {
+                                vendorId: vendorResponse.id,
+                                productId: product.id,
+                                userId: parseInt(localStorage.getItem("groce_user"))
+                            }
 
+                            const fetchOption = {
+                                method: "POST",
+                                headers: {
+                                    "Content-Type": "application/json"
+                                },
+                                body: JSON.stringify(newVendorProduct)
+                            }
 
-            const fetchOption = {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(newVendorProduct)
-            }
-
-            return fetch("http://localhost:8088/vendorProducts", fetchOption)
-        })
-
+                            return fetch("http://localhost:8088/vendorProducts", fetchOption)
+                        }
+                    }
+                }
     }
 
     return (
